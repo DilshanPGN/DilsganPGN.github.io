34ca0557281bd81d00b16222fc7a4bba759d7023
Nipun Dilshan <nidilshan@wiley.com>
2024-03-09 19:19:42 +0530

    adding Hero image component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import HeroImg from '../components/HeroImg/HeroImg'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImg/>
     </div>
   )
 }
