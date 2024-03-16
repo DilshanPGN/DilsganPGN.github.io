0da16bdbc5fe12f8b33c1037743969f97bbfe58c
Nipun Dilshan <nidilshan@wiley.com>
2024-03-16 23:21:29 +0530

    pricing cards

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 import HeroImg2 from '../components/heroimg2/HeroImg2'
+import PricingCard from '../components/pricingCard/PricingCard'
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg2 heading="PROJECTS" text="Some of my most recent works"/>
+      <PricingCard/>
       <Footer/>
     </div>
   )
