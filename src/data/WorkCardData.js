93c51481a1c9333db4a8ce54d0c67c8c18f81b6f
Nipun Dilshan <nidilshan@wiley.com>
2024-03-19 00:14:02 +0530

    Dynamic Card Component

---
+++ src/data/WorkCardData.js
@@ -0,0 +1,27 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a quam a ipsum blandit ultricies non non turpis. Aenean id magna elementum, varius sapien ut, placerat massa. Fusce tincidunt vulputate ligula, sit amet egestas mi. Curabitur auctor sem massa, sed pretium lectus sollicitudin sit amet. Aliquam at ex interdum, pellentesque nisl id, mattis elit. Mauris non erat aliquam, tempor urna vel, aliquet nisl. Vivamus viverra ornare ullamcorper. Suspendisse at mollis libero, ullamcorper fringilla odio. Etiam mollis et orci quis fringilla. Quisque at urna nec risus porttitor facilisis.",
+        view: "https://youtu.be/7PI9Y2xUo08?feature=shared"
+    },
+    {
+        imgsrc: pro2,
+        title: "LMS LMS",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a quam a ipsum blandit ultricies non non turpis. Aenean id magna elementum, varius sapien ut, placerat massa. Fusce tincidunt vulputate ligula, sit amet egestas mi. Curabitur auctor sem massa, sed pretium lectus sollicitudin sit amet. Aliquam at ex interdum, pellentesque nisl id, mattis elit. Mauris non erat aliquam, tempor urna vel, aliquet nisl. Vivamus viverra ornare ullamcorper. Suspendisse at mollis libero, ullamcorper fringilla odio. Etiam mollis et orci quis fringilla. Quisque at urna nec risus porttitor facilisis.",
+        view: "https://youtu.be/7PI9Y2xUo08?feature=shared"
+    },
+    {
+        imgsrc: pro3,
+        title: "Biology Bio",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a quam a ipsum blandit ultricies non non turpis. Aenean id magna elementum, varius sapien ut, placerat massa. Fusce tincidunt vulputate ligula, sit amet egestas mi. Curabitur auctor sem massa, sed pretium lectus sollicitudin sit amet. Aliquam at ex interdum, pellentesque nisl id, mattis elit. Mauris non erat aliquam, tempor urna vel, aliquet nisl. Vivamus viverra ornare ullamcorper. Suspendisse at mollis libero, ullamcorper fringilla odio. Etiam mollis et orci quis fringilla. Quisque at urna nec risus porttitor facilisis.",
+        view: "https://youtu.be/7PI9Y2xUo08?feature=shared"
+    }
+];
+
+
+export default ProjectCardData;
--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 import HeroImg2 from '../components/heroimg2/HeroImg2'
 import PricingCard from '../components/pricingCard/PricingCard'
+import Work from '../components/work/Work'
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg2 heading="PROJECTS" text="Some of my most recent works"/>
+      <Work/>
       <PricingCard/>
       <Footer/>
     </div>
