f7c73a4ab32cde3e7618900a7f111126d7a1e0a3
Nipun Dilshan <nidilshan@wiley.com>
2024-03-19 00:57:39 +0530

    Adding Projects to home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import HeroImg from '../components/heroImg/HeroImg'
 import Footer from '../components/footer/Footer'
+import Work from '../components/work/Work.jsx'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg/>
+      <Work/>
       <Footer/>
     </div>
   )
