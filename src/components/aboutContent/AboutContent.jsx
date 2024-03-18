88fabebfcd78fc1af8e7b51174bee31a5d80dbad
Nipun Dilshan <nidilshan@wiley.com>
2024-03-19 00:38:56 +0530

    About component

---
+++ src/components/aboutContent/AboutContent.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+import "./AboutContent.css"
+import React1 from "../../assets/react1.jpg"
+import React2 from "../../assets/react2.webp"
+
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Who Am I?</h1>
+            <p>Im a full-stack developer. I create responsive secure websites for my clients</p>
+            <Link to="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={React1} alt="true" className="img" />
+                </div>
+                <div className="img-stack bottom">
+                    <img src={React2} alt="true" className="img" />
+                </div>
+            </div>
+        </div>        
+    </div>
+  )
+}
+
+export default AboutContent
